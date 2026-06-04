d602bddf48f71aa326b28affdd5be94653c36ecc
NAVEENKUMAR2003301 <livnaveen@gmail.com>
2026-06-04 12:16:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,161 @@
+// javascript
+
+
+
+// scripting language
+
+// ghjsdbhfbejbsfjsbhfbjhjkfhbshgkj
+// ljhjgkdnfkjnvdjb
+
+
+// // variable
+
+
+// // var
+// // let 
+// // const
+
+
+// // rules : - 
+
+// // var rules 
+
+
+// // 1. declration
+
+// // var a 
+
+// // 2. intialiazation
+
+// var a = 50
+
+
+// // 3. reuse  & 4 . reintialiazation
+
+//     a = 55
+
+// // 5. reDeclration
+
+// var a = 60
+
+// console.log(a)
+
+
+
+// // let - rules 
+
+
+// // 1. declration 
+
+// // let b 
+
+// // 2. intialization 
+
+// let b = 40
+
+
+
+// // 3. reuse & 4. reintialization
+
+// b = 66
+
+// // 5. reDeclration 
+
+// // let b  -- reDeclration not accept
+
+// console.log(b);
+
+
+// // const - constant
+
+
+// // declration & intialiazation
+
+// const c = 10
+
+// // reuse 
+
+//     //   c = 23 // reuse and reInti also not accept
+
+// // reDeclration 
+
+// // const c = 50  -- not accept reDeclration
+
+// console.log(c);
+
+
+
+// // example :- 
+
+
+// let y = 56
+
+//     y = 54
+
+//     console.log(y); // 54
+
+
+// // var d = 4 
+
+// // let y = d 
+
+// // console.log(y); // syntex error
+
+
+// const e = y
+
+    
+
+// console.log(e); // 54
+
+
+    
+
+
+// printing Statement
+
+
+// 1. console.log()
+
+console.log(100);
+
+let a = 50
+
+console.log(a);
+
+
+
+
+// 2. alert()
+
+// alert("hello")
+
+
+// 3. confirm()
+
+// confirm("did you know before program ?")
+
+
+// 4. prompt()
+
+// prompt("what is your name ?")
+
+
+// 5. document.writeln()
+
+// document.writeln("hdfhgjshgdjsd")
+
+
+
+// example 
+
+
+// let userName = prompt("what is your name :")
+
+// console.log(userName);
+
+
+// let userWish = confirm("did you know before program")
+
+// alert(userWish);
+
